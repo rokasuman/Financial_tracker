d7675b03dd2adb7ff4b8c4bb63ec3a96101149b8
rokasuman <roka7362@gmail.com>
2025-11-01 02:49:11 +1100

    set up is done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Button from 'react-bootstrap/Button';
+
+import { Routes,Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+
+ 
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+    </Routes>
+        
+      
+      
+    </>
+  )
+}
+
+export default App
